d0ff098d21eaafd65eef9d8ff5786b9169adcc40
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-05-15 13:54:08 +0200

    Support fewer versions in generate-stackbrew-library.sh
    The Docker folks would like us to clean up our list of "supported"
    image versions. In the past we basically listed all versions that
    we ever released since 3.8, but in reality those would never get
    updated because they're snapshots of the package repositories.
    
    The older tags are still retained by the Docker registry so users
    who rely on those images don't need to worry, except that those images
    won't get rebuilt anymore when the Debian image we base our image on
    is updated. This can be fixed by running `apt-get update/upgrade`.
    
    Going forward we only list the latest, latest-1 and one version
    before the next major version in our "supported" version table which
    get the automatic rebuild treatment.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 set -e
 
+# we want the current latest version, one version before that
+# and the last version before a major version bump
 aliases=(
-	'4.8.0.524  -> 4.8.0 4.8 4 latest'
-	'4.6.2.16   -> 4.6.2 4.6'
-	'4.6.1.5    -> 4.6.1'
-	'4.6.0.245  -> 4.6.0'
-	'4.4.2.11   -> 4.4.2 4.4'
-	'4.4.1.0    -> 4.4.1'
-	'4.4.0.182  -> 4.4.0'
-	'4.2.4.4    -> 4.2.4 4.2'
-	'4.2.3.4    -> 4.2.3'
-	'4.2.2.30   -> 4.2.2'
-	'4.2.1.102  -> 4.2.1'
-	'4.0.5.1    -> 4.0.5 4.0'
+	'5.0.0.100  -> 5.0.0 5.0 5 latest'
+	'4.8.0.524  -> 4.8.0 4.8 4'
 	'3.12.1     -> 3.12.0 3.12 3'
-	'3.10.0     -> 3.10'
-	'3.8.0      -> 3.8'
 )
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -43,16 +33,4 @@ for version in "${versions[@]}"; do
 		echo "$va: ${url}@${commit} $version"
 	done
 
-	for variant in onbuild; do
-		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
-		echo
-		for va in "${versionAliases[@]}"; do
-			if [ "$va" = 'latest' ]; then
-				va="$variant"
-			else
-				va="$va-$variant"
-			fi
-			echo "$va: ${url}@${commit} $version/$variant"
-		done
-	done
 done
